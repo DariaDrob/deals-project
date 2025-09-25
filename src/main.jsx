f7fcf1c3caee08cbc744de3ebbebe9c48821c3e5
DariaDrob <daradrobotenko@gmail.com>
2025-09-25 22:27:50 +0200

    Changes ulpoaded

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './authSlice';
 import App from './App.jsx';
-import './styles.css';
+import '../styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
